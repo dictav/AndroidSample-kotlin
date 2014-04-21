8e4d59a79804dd5d7a07abe9454db5aa2b229dbe
Shintaro Abe <dictav@gmail.com>
2014-04-21 21:19:53 +0900

    Convert MainActivity from java to kotlin

---
+++ AndroidSample/src/main/kotlin/org/jetbrains/kotlin/gradle/androidsample/MainActivity.kt
@@ -0,0 +1,40 @@
+package org.jetbrains.kotlin.gradle.androidsample
+
+import android.content.Intent
+import android.os.Bundle
+import android.app.Activity
+import android.view.Menu
+import android.view.View
+import android.widget.Button
+import org.jetbrains.kotlin.gradle.androidsample.R
+
+/**
+ * Created by dictav on 4/21/14.
+ */
+
+open class MainActivity: Activity() {
+
+    protected override fun onCreate(savedInstanceState: Bundle?): Unit {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var next: Button = findViewById(R.id.Button01) as Button
+        next.setOnClickListener(object: View.OnClickListener {
+            public override fun onClick(view: View): Unit {
+                /* ??? why don't work?
+                 * val intent = Intent(view.getContext(), javaClass<MainActivity2>())
+                 */
+                val intent = Intent()
+                intent.setClassName("org.jetbrains.kotlin.gradle.androidsample","org.jetbrains.kotlin.gradle.androidsample.MainActivity2");
+                startActivityForResult(intent,0)
+            }
+        })
+    }
+
+    public override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true
+    }
+
+}
+
