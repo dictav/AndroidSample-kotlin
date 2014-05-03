b20cbb32ed44362ba194f1b4478920890c361f69
sambaiz <godgourd@gmail.com>
2014-05-03 22:54:50 +0900

    take a picture

--- AndroidSample/src/main/kotlin/org/jetbrains/kotlin/gradle/androidsample/PickphotoActivity.kt
+++ AndroidSample/src/main/kotlin/org/jetbrains/kotlin/gradle/androidsample/PickphotoActivity.kt
@@ -11,13 +11,16 @@ import java.io.InputStream
 import android.widget.ImageView
 import android.media.ExifInterface
 import android.net.Uri
-import android.util.Log
 import android.graphics.Matrix
 import android.graphics.Bitmap
+import android.content.ContentValues
+import android.provider.MediaStore
 
 open class PickphotoActivity: Activity() {
 
-    private val REQUEST_GALLERY = 0;
+    private val REQUEST_GALLERY = 0
+    private val REQUEST_CAMERA = 1
+    private var mImageUri: Uri? = null
 
     protected override fun onCreate(savedInstanceState: Bundle?): Unit {
         super.onCreate(savedInstanceState)
@@ -32,36 +35,26 @@ open class PickphotoActivity: Activity() {
                 startActivityForResult(intent, REQUEST_GALLERY);
             }
         })
+        var button2 : Button = findViewById(R.id.button2) as Button
+        button2.setOnClickListener(object: View.OnClickListener {
+            public override fun onClick(view: View): Unit {
+                onExtraClick(view);
+            }
+        })
     }
 
     override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?) {
         if(requestCode == REQUEST_GALLERY && resultCode == Activity.RESULT_OK) {
-            val options : BitmapFactory.Options = BitmapFactory.Options();
-            options.inSampleSize = 8; //load scales bitmaps to memory
             val ins : InputStream? = getContentResolver()?.openInputStream(data?.getData()!!);
-            val img = BitmapFactory.decodeStream(ins);
-            ins?.close();
-
-            val columns = array<String>("_data") //kotin can't find "MediaStore.Images.Media.DATA"
-            val c = getContentResolver()?.query(data?.getData() as Uri, columns, null, null, null)
-            c?.moveToFirst()
-            val exifInterface = ExifInterface(c?.getString(0)!!)
-            val exifR : Int = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
-            val orientation : Float =
-                    when (exifR) {
-                        ExifInterface.ORIENTATION_ROTATE_90 ->  90f
-                        ExifInterface.ORIENTATION_ROTATE_180 -> 180f
-                        ExifInterface.ORIENTATION_ROTATE_270 -> 270f
-                        else -> 0f
-                    }
-
-            val mat : Matrix? = Matrix()
-            mat?.postRotate(orientation)
-            val turnedBitmap = Bitmap.createBitmap(img as Bitmap, 0, 0, img?.getWidth() as Int,
-                    img?.getHeight() as Int, mat, true)
+            val img : Bitmap? = BitmapFactory.decodeStream(ins);
 
             // 選択した画像を表示
-            (findViewById(R.id.imageView) as ImageView).setImageBitmap(turnedBitmap);
+            (findViewById(R.id.imageView) as ImageView).setImageBitmap(pictureTurn(img, data?.getData()));
+        }else if(requestCode == REQUEST_CAMERA && resultCode == Activity.RESULT_OK) {
+            val ins : InputStream? = getContentResolver()?.openInputStream(mImageUri!!);
+            val img : Bitmap? = BitmapFactory.decodeStream(ins);
+            ins?.close();
+            (findViewById(R.id.imageView) as ImageView).setImageBitmap(pictureTurn(img, mImageUri));
         }
     }
 
@@ -69,6 +62,40 @@ open class PickphotoActivity: Activity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_activity2, menu);
         return true
+    }
+
+    public fun onExtraClick(view: View) {
+        val filename = "" +System.currentTimeMillis() + ".jpg"
+
+        val values = ContentValues()
+        values.put(MediaStore.MediaColumns.TITLE, filename)
+        values.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        mImageUri = getContentResolver()?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val intent = Intent()
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri)
+        startActivityForResult(intent, REQUEST_CAMERA)
+    }
+
+    private fun pictureTurn(img : Bitmap?, uri : Uri?) : Bitmap {
+        val columns = array<String>(MediaStore.MediaColumns.DATA)
+        val c = getContentResolver()?.query(uri!!, columns, null, null, null)
+        c?.moveToFirst()
+        val exifInterface = ExifInterface(c?.getString(0)!!)
+        val exifR : Int = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+        val orientation : Float =
+                when (exifR) {
+                    ExifInterface.ORIENTATION_ROTATE_90 ->  90f
+                    ExifInterface.ORIENTATION_ROTATE_180 -> 180f
+                    ExifInterface.ORIENTATION_ROTATE_270 -> 270f
+                    else -> 0f
+                }
+
+        val mat : Matrix? = Matrix()
+        mat?.postRotate(orientation)
+        return Bitmap.createBitmap(img as Bitmap, 0, 0, img?.getWidth() as Int,
+                img?.getHeight() as Int, mat, true)
     }
 
 }
