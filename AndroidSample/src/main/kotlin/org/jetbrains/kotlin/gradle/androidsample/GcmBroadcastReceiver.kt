d9f526437ffcfa169852aeed53d1c12c7ced50ae
sambaiz <godgourd@gmail.com>
2014-05-05 16:26:39 +0900

    success receive message from GCM

--- AndroidSample/src/main/kotlin/org/jetbrains/kotlin/gradle/androidsample/GcmBroadcastReceiver.kt
+++ AndroidSample/src/main/kotlin/org/jetbrains/kotlin/gradle/androidsample/GcmBroadcastReceiver.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.ComponentName
 import android.support.v4.content.WakefulBroadcastReceiver
 import android.app.Activity
+import android.util.Log
 
 class GcmBroadcastReceiver : WakefulBroadcastReceiver() {
 
