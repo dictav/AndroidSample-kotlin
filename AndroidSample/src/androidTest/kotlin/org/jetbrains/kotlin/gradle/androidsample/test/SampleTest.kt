083635795685149cb39828fc3205b797183e0b15
Shintaro Abe <dictav@gmail.com>
2014-05-05 16:44:25 +0900

    Implement aws s3

--- AndroidSample/src/androidTest/kotlin/org/jetbrains/kotlin/gradle/androidsample/test/SampleTest.kt
+++ AndroidSample/src/androidTest/kotlin/org/jetbrains/kotlin/gradle/androidsample/test/SampleTest.kt
@@ -12,6 +12,17 @@ import android.widget.Button
 import org.msgpack.MessagePack
 import redis.clients.jedis.Jedis
 import com.android.vending.billing.IInAppBillingService
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.regions.Regions
+import com.amazonaws.regions.Region
+import org.apache.commons.logging.Log
+import android.util.Log
+import com.amazonaws.services.s3.model.Bucket
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
+import com.amazonaws.services.s3.model.ResponseHeaderOverrides
+import com.amazonaws.services.s3.model.PutObjectRequest
+import java.io.File
 
 /**
  * Created by dictav on 4/22/14.
@@ -44,9 +55,31 @@ class SampleTest() : ActivityInstrumentationTestCase2<MainActivity?>("org.jetbra
     /* Redis の使い方 */
     public fun testRedis() {
         val jedis = Jedis("10.0.2.2")
+        assertNotNull(jedis)
         jedis.set("hoge", "10")
         assertEquals(jedis.get("hoge"), "10")
     }
 
+    /* AWS S3 の使い方 */
+    public fun testAWS() {
+        val accessKey = myActivity?.getString(R.string.aws_access_key)
+        val secretKey = myActivity?.getString(R.string.aws_secret_key)
+        val bucketName = "doya"
+        val s3Client = AmazonS3Client( BasicAWSCredentials(accessKey, secretKey) )
+        s3Client.setRegion(Region.getRegion(Regions.AP_NORTHEAST_1))
+        val buckets = s3Client.listBuckets()
+        assertNotNull(buckets)
+        assertTrue( buckets!!.size > 0 )
+
+        // upload
+        val fileName = "piyo.txt"
+        val path = myActivity?.getFilesDir()
+        val stream = myActivity?.openFileOutput(fileName, 0)
+        stream?.write("hello, aws".toByteArray())
+        stream?.close()
+
+        val file = File(path,fileName)
+        val por = PutObjectRequest("doya",fileName,file)
+        s3Client.putObject(por)
     }
 }
